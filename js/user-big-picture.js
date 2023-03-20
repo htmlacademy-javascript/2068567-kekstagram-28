20c32d2fe8ab2c51fa8af6d243f8e1eeb6de0172
skate090961 <m4xwork@yandex.ru>
2023-03-20 18:06:41 +0400

    добавит функционал вывода комментариев по кнопке Загрузить еще

--- js/user-big-picture.js
+++ js/user-big-picture.js
@@ -1,6 +1,7 @@
 import './similar-pictures.js';
 import {isEscapeKey} from './util-data.js';
-import {renderPhotoData} from './render-big-picture.js';
+import {renderPhotoData, renderSocialComment} from './render-big-picture.js';
+import {similarPictureElements} from './similar-pictures.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const body = document.querySelector('body');
@@ -8,18 +9,20 @@ const pictures = body.querySelector('.pictures');
 const socialCommentCount = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
+const commentsCount = bigPicture.querySelector('.comments-count');
+const COMMENT_AMOUNT = 5;
+let commentCount = COMMENT_AMOUNT;
 
 const changeClassClose = () => {
   bigPicture.classList.add('hidden');
-  socialCommentCount.classList.remove('hidden');
-  commentsLoader.classList.remove('hidden');
   body.classList.remove('modal-open');
+  commentsLoader.classList.remove('hidden');
+  socialCommentCount.textContent = `${COMMENT_AMOUNT} из ${commentsCount.textContent} комментариев`;
+  commentCount = COMMENT_AMOUNT;
 };
 
 const changeClassOpen = () => {
   bigPicture.classList.remove('hidden');
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
   body.classList.add('modal-open');
 };
 
@@ -39,13 +42,36 @@ const closeBigPicture = () => {
   changeClassClose();
 };
 
+const findPhotoPath = (evt) => {
+  const correctPath = (evt.target.src).split('/').slice(-2).join('/');
+  return similarPictureElements.find((pictureElement) => pictureElement.url === correctPath);
+};
+
 pictures.addEventListener('click', (evt) => {
   if (evt.target.closest('.picture__img')) {
     openBigPicture();
-    renderPhotoData(evt);
+    renderSocialComment(findPhotoPath(evt));
+    renderPhotoData(findPhotoPath(evt));
+  }
+  const socials = bigPicture.querySelectorAll('.social__comment');
+  for (let i = 0; i < COMMENT_AMOUNT; i++) {
+    socials[i].classList.remove('hidden');
   }
 });
 
 bigPictureCancel.addEventListener('click', () => {
   closeBigPicture();
+});
+
+commentsLoader.addEventListener('click', () => {
+  commentCount += 5;
+  const socialComments = bigPicture.querySelectorAll('.social__comment');
+  const socialCommentsArray = Array.from(socialComments);
+  const commentItem = socialCommentsArray.slice(0, commentCount);
+  commentItem.forEach((element) => element.classList.remove('hidden'));
+  if (commentItem.length === socialComments.length) {
+    commentsLoader.classList.add('hidden');
+    commentCount = COMMENT_AMOUNT;
+  }
+  socialCommentCount.textContent = `${commentItem.length} из ${commentsCount.textContent} комментариев`;
 });
